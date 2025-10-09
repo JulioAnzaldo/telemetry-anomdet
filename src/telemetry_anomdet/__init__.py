6a41e0f54ea965851c1a4f91ef8d45bcc1f64c09
Julio Anzaldo <anzaldojuliojesus@gmail.com>
2025-10-08 21:06:26 -0700

    classifiers and __init__

---
+++ src/telemetry_anomdet/__init__.py
@@ -0,0 +1,14 @@
+# telemetry_anomdet/__init__.py
+
+"""
+Telemetry Anomaly Detection Toolkit
+
+This package provides tools for:
+- Preprocessing telemetry data
+- Extracting features
+- Running unsupervised anomaly detection models
+
+Usage:
+    import telemetry_anomdet
+    # Access modules like telemetry_anomdet.preprocessing
+"""
